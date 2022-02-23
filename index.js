e763c6c20ac52ebd68882e2aa6a53be8ca1aeb0a
Konfitysha <79853863+Konfitysha@users.noreply.github.com>
2022-02-23 20:05:40 +0300

    создание странички( описание + 2 картинки)

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ server.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
+// Это страница code vein
+server.get("/code-vein", (req, res) => {
+  res.sendFile(__dirname + "/code_vein.html")
+})
+
 server.listen(port, () => console.log('Сервер запущен'));
 
 
