b1255430f4da20bb90f481d1baf126945869cc05
Dmitry Dudkin <dimondudkin@gmail.com>
2022-03-02 19:14:19 +0300

    Добавлена папка для файлов

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+server.use(express.static('files'))
+
 // Это главная страница сайта
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
 
-// game
-server.get("/game1", (req, res) => {
-  res.send('Тут будет описание game1')
-  // res.sendFile(__dirname + "/index.html")
+// Это обработчик страницы game_test.html
+server.get("/test_game", (req, res) => {
+  res.sendFile(__dirname + "/test_game.html")
 })
-
-
-
 
 server.listen(port, () => console.log('Сервер запущен'));
 
