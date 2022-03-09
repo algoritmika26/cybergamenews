87c50cb164acfb500a9974d2cb6c63c5b4316fe9
Pfeneeks3105 <99820473+Pfeneeks3105@users.noreply.github.com>
2022-03-09 19:10:06 +0300

    Breathedge начало

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const server = express();
 const port = 3000;
 
+server.use(express.static("files"))
+
 // Это главная страница сайта
 server.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html")
 })
+
+// не главная страница
+server.get("/braethedge", (req, res) => {
+  res.sendFile(__dirname + "/breathedge.html")
+})
+
 
 server.listen(port, () => console.log('Сервер запущен'));
 
