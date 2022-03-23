4415ee7feb6bf0edc1a4946ef8e45a163e0d1d2d
_UMBRELLA <89023083+TheOre0L@users.noreply.github.com>
2022-03-23 17:03:31 +0300

    Add files via upload

--- models/posts.js
+++ models/posts.js
@@ -1,19 +1,6 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
-
-const postSchema = new Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  text: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
-})
-
-module.exports = mongoose.model('posts', postSchema)
+const mongoose = require('mongoose');
+const postsSchema = mongoose.Schema({
+	title: String,
+	text: String,
+});
+module.exports = mongoose.model('posts', postsSchema);
--- models/users.js
+++ models/users.js
@@ -4,9 +4,7 @@ const userSchema = mongoose.Schema({
 	password: String,
 	Roles: {type: String, default: 'USER'},
 	user_id: String,
-	about: {type: String, default: 'Отсутствует'},
 	secret_id: String,
 	dateRegist: {type: Date, default: Date.now()},
-	image_url: {type: String, default: 'https://images-ext-1.discordapp.net/external/5bGWfr6UHbV89H_FJ-GvXmk-OzxRUpYsAwmpo_ZzdRg/https/www.imgonline.com.ua/examples/bee-on-daisy.jpg?width=1035&height=677'}
-})
+});
 module.exports = mongoose.model('usersSite', userSchema);
